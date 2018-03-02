19b98ae616b7bd8cc755677d0a9381c09292b510
Kris Scheppe <kris@scheppe.us>
2018-03-02 16:31:00 -0500

    Currency converter model, controller, routes, request, and views.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'CurrencyController@index')->name('index');
+Route::post('/convert','CurrencyController@convert')->name('convert');
